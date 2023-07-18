601cf45e2187b4a878c218b6dd81d492fbee04fe
“tamerajaj” <tamer.ajaj@gmail.com>
2023-07-18 12:06:32 +0200

    remove unused functions

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,11 +1,8 @@
 import os
 import sys
 
-import optuna
 import pandas as pd
 from dotenv import load_dotenv
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
 
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
@@ -48,54 +45,3 @@ def preprocess(df: pd.DataFrame):  # , dv: DictVectorizer, fit_dv: bool = False)
     dicts = df[categorical + numerical].to_dict(orient="records")
 
     return dicts
-
-
-def optimize_hyperparameters(self, X_train, y_train, X_test, y_test):
-    """
-    Perform hyperparameter optimization with Optuna.
-    """
-
-    def objective(trial):
-        # Define the search space for hyperparameters
-        criterion = trial.suggest_categorical("criterion", ["gini", "entropy"])
-        max_depth = trial.suggest_int("max_depth", 2, 10)
-        min_samples_split = trial.suggest_int("min_samples_split", 2, 20)
-        min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 10)
-
-        # Create a decision tree classifier with the hyperparameters
-        # TODO: change parameters
-        dec_tree = RandomForestRegressor(
-            criterion=criterion,
-            max_depth=max_depth,
-            min_samples_split=min_samples_split,
-            min_samples_leaf=min_samples_leaf,
-        )
-
-        # Train the classifier
-        dec_tree.fit(X_train, y_train)
-
-        # Make predictions on the test set
-        y_pred = dec_tree.predict(X_test)
-
-        # Calculate the accuracy
-        accuracy = mean_squared_error(y_test, y_pred)
-
-        return accuracy
-
-    study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100)
-
-    # Print the best hyperparameters and the corresponding accuracy
-    best_trial = study.best_trial
-    print("Best hyperparameters:", best_trial.params)
-    print("Best accuracy:", best_trial.value)
-    return best_trial.params, study
-
-
-def train_model_with_best_hyperparameters(model, X_train, y_train, best_params):
-    """
-    Train a decision tree model with the best hyperparameters.
-    """
-    model = model(**best_params)
-    model.fit(X_train, y_train)
-    return model
